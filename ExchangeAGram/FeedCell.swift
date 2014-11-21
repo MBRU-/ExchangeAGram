19fb12f10f3bc91e24ea08787e7bee203611a5d1
Martin Brunner <mbru_mbrux@me.com>
2014-11-21 08:21:16 +0100

    V001

---
+++ ExchangeAGram/FeedCell.swift
@@ -0,0 +1,16 @@
+//
+//  FeedCell.swift
+//  ExchangeAGram
+//
+//  Created by Martin Brunner on 20.11.14.
+//  Copyright (c) 2014 Martin Brunner. All rights reserved.
+//
+
+import UIKit
+
+class FeedCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var captionLabel: UILabel!
+    
+}
